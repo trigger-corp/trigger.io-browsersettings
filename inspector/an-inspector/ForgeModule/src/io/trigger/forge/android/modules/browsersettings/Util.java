b310df98bda9e6e040412b8ef2b20d4f0423fbf1
Antoine van Gelder <antoine@trigger.io>
2016-02-05 09:59:52 +0200

    Check for Android API level 17 or higher when calling setAutoplayVideo

--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/browsersettings/Util.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/browsersettings/Util.java
@@ -3,13 +3,18 @@ package io.trigger.forge.android.modules.browsersettings;
 import android.os.Build;
 import android.webkit.CookieManager;
 import io.trigger.forge.android.core.ForgeApp;
+import io.trigger.forge.android.core.ForgeLog;
 
 public class Util {
 	public static void setInlineVideo(boolean forceInlineVideo) {	    
 		//not required for android, video plays inline by default	
 	}
 	public static void setAutoplayVideo(boolean autoplayVideo) {
-		ForgeApp.getActivity().webView.getSettings().setMediaPlaybackRequiresUserGesture(!autoplayVideo);
+		if (Build.VERSION.SDK_INT >= 17 /* Build.VERSION_CODES.JELLY_BEAN_MR1 */) {		
+			ForgeApp.getActivity().webView.getSettings().setMediaPlaybackRequiresUserGesture(!autoplayVideo);
+		} else {
+			ForgeLog.w("browsersettings.setAutoPlayVideo() is only supported on Android API level 17 (Jelly Bean MR1) or higher.");
+		}
 	}
 	public static void setAcceptCookies(boolean acceptCookies){		
 		CookieManager cookieManager = CookieManager.getInstance();
@@ -17,6 +22,5 @@ public class Util {
 		if (Build.VERSION.SDK_INT >= 21 /* Build.VERSION_CODES.LOLLIPOP */) {
 			cookieManager.setAcceptThirdPartyCookies(ForgeApp.getActivity().webView, acceptCookies);
 		}
-		
 	}
 }
