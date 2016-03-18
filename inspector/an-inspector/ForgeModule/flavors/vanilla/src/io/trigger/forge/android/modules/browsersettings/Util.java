7137a3d16825cfc9bd0ceb264e10b31253c95a7d
Antoine van Gelder <antoine@trigger.io>
2016-03-18 10:53:53 +0200

    Support Crosswalk 17.46.448.10

--- inspector/an-inspector/ForgeModule/flavors/vanilla/src/io/trigger/forge/android/modules/browsersettings/Util.java
+++ inspector/an-inspector/ForgeModule/flavors/vanilla/src/io/trigger/forge/android/modules/browsersettings/Util.java
@@ -9,13 +9,15 @@ public class Util {
 	public static void setInlineVideo(boolean forceInlineVideo) {	    
 		//not required for android, video plays inline by default	
 	}
+
 	public static void setAutoplayVideo(boolean autoplayVideo) {
 		if (Build.VERSION.SDK_INT >= 17 /* Build.VERSION_CODES.JELLY_BEAN_MR1 */) {		
-			ForgeApp.getActivity().webView.getSettings().setMediaPlaybackRequiresUserGesture(!autoplayVideo);
+			ForgeApp.getActivity().webView.getSettingsInternal().setMediaPlaybackRequiresUserGesture(!autoplayVideo);
 		} else {
 			ForgeLog.w("browsersettings.setAutoPlayVideo() is only supported on Android API level 17 (Jelly Bean MR1) or higher.");
 		}
 	}
+
 	public static void setAcceptCookies(boolean acceptCookies){		
 		CookieManager cookieManager = CookieManager.getInstance();
 		cookieManager.setAcceptCookie(acceptCookies);
