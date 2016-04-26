57cb114b302490dbb40781069e12c245d1df188e
Antoine van Gelder <antoine@trigger.io>
2016-04-26 11:28:28 +0200

    Fixed: Setting auto_play_video to true could cause crash on startup with Android 4.1 devices.

--- inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/browsersettings/EventListener.java
+++ inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/browsersettings/EventListener.java
@@ -7,20 +7,23 @@ import io.trigger.forge.android.core.ForgeEventListener;
 import android.os.Bundle;
 
 public class EventListener extends ForgeEventListener {
-	
+
 	public void onCreate(Bundle savedInstanceState) {
 		JsonObject config = (JsonObject)ForgeApp.configForModule("browsersettings");
-				
+
 		if (config.has("media_playback") && config.getAsJsonObject("media_playback").has("inline_video") && config.getAsJsonObject("media_playback").get("inline_video").getAsBoolean()) {
 			Util.setInlineVideo(true);
 		}
 		if (config.has("media_playback") && config.getAsJsonObject("media_playback").has("autoplay_video") && config.getAsJsonObject("media_playback").get("autoplay_video").getAsBoolean()) {
-			Util.setAutoplayVideo(true);
+			ForgeApp.getActivity().runOnUiThread(new Runnable() {
+				public void run() {
+					Util.setAutoplayVideo(true);
+				}
+			});
 		}
 		if (config.has("accept_cookies") && config.get("accept_cookies").getAsBoolean()) {
 			Util.setAcceptCookies(true);
-		}		
+		}
 	}
-	
-}
 
+}
